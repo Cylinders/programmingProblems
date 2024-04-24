fed3ac92237cfcddf914aa4a0423919fb33d5bca
ruthvik <ruthvikotravnkatesan@gmail.com>
2024-04-23 20:49:14 -0400

    test gcc linux

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+
+
+// for testing clang and / or gcc on new setups. 
+
+int main() {
+
+    cout << "THIS SHOULD PRINT FOR TESTING CLANG"; 
+    return 0; 
+
+}
